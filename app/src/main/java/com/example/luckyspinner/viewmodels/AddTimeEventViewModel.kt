9441de687f6d4b62c992fe574eea1104a50884ff
Minh <minhtvgch18534@fpt.edu.vn>
2024-01-16 13:29:14 +0700

    minh

--- app/src/main/java/com/example/luckyspinner/viewmodels/AddTimeEventViewModel.kt
+++ app/src/main/java/com/example/luckyspinner/viewmodels/AddTimeEventViewModel.kt
@@ -57,6 +57,7 @@ class AddTimeEventViewModel : ViewModel() {
                         }
                     }
                     memberList.value = list
+                    println("Here come list $list")
                     if (isNewEvent){
                         allCheckboxMember(false)
                     }
